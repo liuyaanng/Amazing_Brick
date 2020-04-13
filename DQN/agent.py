bd7c6ca0db47f66c0e82f0f144d6e8d6737b387e
liuyaanng <1757465118@qq.com>
2020-04-13 17:50:59 +0800

    add agent->nextaction function

--- DQN/agent.py
+++ DQN/agent.py
@@ -10,19 +10,39 @@ from skimage import io
 import numpy as np
 import random
 from collections import deque
-
-
+from tensorflow.keras.optimizers import Adam
+from DQN.DQN import *
 class DQNagent():
 
     """Docstring for DQNagent. """
 
     def __init__(self, **kwargs):
         """TODO: to be defined. """
+        
+        self.discount_factor = 0.99
+        self.epsilon = 0.1
+        self.init_epsilon = 0.1
+        self.final_epsilon = 1e-4
+
+        self.num_observes = 3200
+        self.num_explores = 3e6
+        self.num_iters = 0
+
+        self.num_actions = 3
         self.num_input_frames = 4
         self.replay_memory_record = deque()
         self.replay_memory_size = 5e4
-        self.img_size = (80, 80)
+        self.image_size = (80, 80)
         self.input_image = None
+
+        self.batch_size = 32
+
+
+        # create q network
+        self.DQN_model = Build_Q_network(image_size = self.image_size, channels = self.num_input_frames, num_actions = self.num_actions)
+
+        self.optimizer = Adam(lr = 1e-4)
+        self.DQN_model.compile(loss = 'mse', optimizer = self.optimizer)
 
     def NextAction(self, reward):
         """TODO: Docstring for NextAction.
@@ -31,7 +51,41 @@ class DQNagent():
         :returns: TODO
 
         """
-        pass
+        # As the training progresses, ε control of actions becomes less and less important
+        if self.epsilon > self.final_epsilon and self.num_iters > self.num_observes:
+            self.epsilon -= (self.init_epsilon - self.final_epsilon ) / self.num_explores
+        self.num_iters += 1
+
+        # Make decision
+        # ε-greddy
+        if random.random() <= self.epsilon:
+            action = random.choice([0, 1, 2])
+        else:
+            q = self.DQN_model.predict(self.input_image)
+            action = np.argmax(q)
+
+        # Train model
+        loss = 0
+        if self.num_iters > self.num_observes:
+            # Sample values
+            minibatch = random.sample(self.replay_memory_record, self.batch_size)
+
+            # Find every label list
+            states, actions, rewards, states_next, is_game_running = zip(*minibatch)
+            # Concatenate arrays
+            states = np.concatenate(states)
+            states_next = np.concatenate(states_next)
+            targets = self.DQN_model.predict(states_next)
+            targets[range[32], action] = rewards + self.discount_factor *np.max(self.DQN_model.predict(states_next), axis = 1) * is_game_running
+            loss = self.DQN_model.train_on_batch(states, targets)
+
+            if self.num_iters % self.save_interval == 0:
+                self.SaveModel(self.backup_path)
+
+
+
+
+
     def record(self, action, reward, score, is_game_running, image):
         """TODO: Docstring for record.
 
@@ -44,7 +98,7 @@ class DQNagent():
         """
 
         # preprocess the image. image: 80x80
-        image = self.preprocess(image, self.img_size)
+        image = self.preprocess(image, self.image_size)
         
         # record the scene and corresponding info
         if self.input_image is None:
